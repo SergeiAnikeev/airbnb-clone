f92638b08e6cd9ba4e0781f1d2d4a75ad11be6c1
Sergei Anikeev <anikeevsu@gmail.com>
2022-05-02 13:33:12 +1200

    add card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,20 @@
 import React from "react"
+import star from '../images/star.png';
+import photo from '../images/katie-zaferes.png';
 
 export default function Card() {
     return (
-        <h1>Replace this</h1>
+        <div className="card">
+        <img src={photo} className="card-image" />
+        <div className="card-stats">
+            <img src={star} className="card-star" />
+            <span>5.0</span>
+            <span className="grey">(6) • </span>
+            <span className="grey">USA</span>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
+        </div>
+    </div>
+
     )
 }
