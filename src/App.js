15b4d31ab8b4b8949c2bbfa97dc6cc138cbe1a6a
Sergei Anikeev <anikeevsu@gmail.com>
2022-05-03 15:36:52 +1200

    added props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ function App() {
     <div>
       <Navbar/>
       <Hero />
-      <Card />
+      <Card 
+        img="../images/katie-zaferes.png"
+        rating={5.0}
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,17 +2,18 @@ import React from "react"
 import star from '../images/star.png';
 import photo from '../images/katie-zaferes.png';
 
-export default function Card() {
+export default function Card(props) {
+    //console.log(props)
     return (
         <div className="card">
-        <img src={photo} className="card-image" />
+        <img src={`../images/${props.img}`} className="card-image" />
         <div className="card-stats">
             <img src={star} className="card-star" />
-            <span>5.0</span>
-            <span className="grey">(6) • </span>
-            <span className="grey">USA</span>
+            <span>{props.rating}</span>
+            <span className="grey">({props.reviewCount}) • </span>
+            <span className="grey">{props.country}</span>
             <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     </div>
 
