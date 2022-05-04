1353d267af9871acd5013495edccd0afdee274fc
Sergei Anikeev <anikeevsu@gmail.com>
2022-05-04 21:04:02 +1200

    add data

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import Hero from "./components/Hero"
 import Card from "./components/Card"
 import photo from './images/katie-zaferes.png';
 import './App.css';
+import data from './data';
 
 function App() {
+
+  const cards = data.map(item => 
+      {
+        return (
+          <Card 
+              img={item.coverImg}
+              rating={item.stats.rating}
+              reviewCount={item.stats.reviewCount}
+              location={item.location}
+              title={item.title}
+              price={item.price}
+          />
+        )
+      }
+    )
+
   return (
     <div>
       <Navbar/>
       <Hero />
-      <Card 
-        img={photo}
-        rating={5.0}
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      {cards}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -11,7 +11,7 @@ export default function Card(props) {
             <img src={star} className="card-star" />
             <span>{props.rating}</span>
             <span className="grey">({props.reviewCount}) • </span>
-            <span className="grey">{props.country}</span>
+            <span className="grey">{props.location}</span>
             <p>Life Lessons with Katie Zaferes</p>
             <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
